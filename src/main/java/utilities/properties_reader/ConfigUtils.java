2e1de2283bd513672912f3ed7964e123caed56e5
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-20 01:53:58 +0200

    Update ConfigUtils.java

--- src/main/java/utilities/properties_reader/ConfigUtils.java
+++ src/main/java/utilities/properties_reader/ConfigUtils.java
@@ -2,14 +2,14 @@ package utilities.properties_reader;
 
 public class ConfigUtils {
     private static final String CONFIG_PROPERTIES_FILE_PATH = "/src/main/resources/Config.properties";
-    private static String URI;
+    private static String URl;
 
     private static void setConfigProperties() {
-        URI = PropertiesDataManager.getProperty("nopCommerce_URL", CONFIG_PROPERTIES_FILE_PATH);
+        URl = PropertiesDataManager.getProperty("nopCommerce_URL", CONFIG_PROPERTIES_FILE_PATH);
     }
 
     public static String get_URI() {
         setConfigProperties();
-        return URI;
+        return URl;
     }
 }
