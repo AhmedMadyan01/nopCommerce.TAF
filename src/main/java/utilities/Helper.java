a07a61fd6c33d98aa46bac74f58b10f5e08c1a94
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-20 00:50:51 +0200

    Update Helper.java

--- src/main/java/utilities/Helper.java
+++ src/main/java/utilities/Helper.java
@@ -4,17 +4,11 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import pages.PageBase;
 
 import java.io.File;
 import java.io.IOException;
 
-public class Helper extends PageBase {
-
-    public Helper(WebDriver driver) {
-        super(driver);
-    }
-
+public class Helper {
     public static void captureScreenshot(WebDriver driver, String screenshotName) throws IOException {
         TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
         File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
